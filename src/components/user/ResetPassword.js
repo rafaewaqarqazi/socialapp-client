773c0770573de15a9d8bdc5b118e62c7029ff325
Rafae Waqar Qazi <rafaewaqar@gmail.com>
2019-07-29 12:52:36 +0500

    Refactored code to use Hooks

--- src/components/user/ResetPassword.js
+++ src/components/user/ResetPassword.js
@@ -1,74 +1,69 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { resetPassword } from "../../auth";
 
-class ResetPassword extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            newPassword: "",
-            message: "",
-            error: ""
-        };
-    }
+const ResetPassword = props => {
+    const [state, setState] = useState({
+        newPassword: '',
+        message: '',
+        error: ''
+    });
 
-    resetPassword = e => {
+    const resetPasswordFunc = e => {
         e.preventDefault();
-        this.setState({ message: "", error: "" });
+        setState({...state, message: "", error: "" });
 
         resetPassword({
-            newPassword: this.state.newPassword,
-            resetPasswordLink: this.props.match.params.resetPasswordToken
+            newPassword: state.newPassword,
+            resetPasswordLink: props.match.params.resetPasswordToken
         }).then(data => {
             if (data.error) {
                 console.log(data.error);
-                this.setState({ error: data.error });
+                setState({...state, error: data.error });
             } else {
                 console.log(data.message);
-                this.setState({ message: data.message, newPassword: "" });
+                setState({...state, message: data.message, newPassword: "" });
             }
         });
     };
 
-    render() {
-        return (
-            <div className="container">
-                <h2 className="mt-5 mb-5">Reset your Password</h2>
+    return (
+        <div className="container">
+            <h2 className="mt-5 mb-5">Reset your Password</h2>
 
-                {this.state.message && (
-                    <h4 className="bg-success">{this.state.message}</h4>
-                )}
-                {this.state.error && (
-                    <h4 className="bg-warning">{this.state.error}</h4>
-                )}
+            {state.message && (
+                <h4 className="bg-success">{state.message}</h4>
+            )}
+            {state.error && (
+                <h4 className="bg-warning">{state.error}</h4>
+            )}
 
-                <form>
-                    <div className="form-group mt-5">
-                        <input
-                            type="password"
-                            className="form-control"
-                            placeholder="Your new password"
-                            value={this.state.newPassword}
-                            name="newPassword"
-                            onChange={e =>
-                                this.setState({
-                                    newPassword: e.target.value,
-                                    message: "",
-                                    error: ""
-                                })
-                            }
-                            autoFocus
-                        />
-                    </div>
-                    <button
-                        onClick={this.resetPassword}
-                        className="btn btn-raised btn-primary"
-                    >
-                        Reset Password
-                    </button>
-                </form>
-            </div>
-        );
-    }
-}
+            <form>
+                <div className="form-group mt-5">
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Your new password"
+                        value={state.newPassword}
+                        name="newPassword"
+                        onChange={e =>
+                            setState({
+                                newPassword: e.target.value,
+                                message: "",
+                                error: ""
+                            })
+                        }
+                        autoFocus
+                    />
+                </div>
+                <button
+                    onClick={resetPasswordFunc}
+                    className="btn btn-raised btn-primary"
+                >
+                    Reset Password
+                </button>
+            </form>
+        </div>
+    );
+};
 
 export default ResetPassword;
